88c1f73217097894b24a61cae749aa9fb520c48a
sb <237886466@qq.com>
2018-09-10 23:56:57 +0800

    test init

---
+++ BigInteger/test-biginteger.cpp
@@ -0,0 +1,64 @@
+//
+// Created by sb on 18-9-5.
+//
+
+
+#include "biginteger.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void TestStream()
+{
+    BigInteger a , b;
+    cin >> a >> b;
+    cout << a << endl << b << endl;
+}
+
+void TestAdd()
+{
+    BigInteger a , b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
+
+void TestSub()
+{
+    BigInteger a , b;
+    cin >> a >> b;
+    cout << a - b << endl;
+}
+
+void TestMul()
+{
+    BigInteger a , b;
+    cin >> a >> b;
+    cout << a * b << endl;
+}
+
+void TestDiv()
+{
+    BigInteger a , b;
+    cin >> a >> b;
+    cout << a / b << endll
+}
+
+void TestMod()
+{
+    BigInteger a,  b;
+    cin >> a >> b;
+    cout << a % b << endl;
+}
+
+void TestEqual()
+{
+    BigInteger a , b;
+    cin >> a >> b;
+    cout << (a == b) << endl;
+}
+
+
+int main()
+{
+
+}
